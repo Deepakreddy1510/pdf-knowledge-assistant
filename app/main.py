a2fd6288453b7efc96c97b1e8633b27274b79067
P Deepak Reddy <deepakreddy1510@gmail.com>
2026-07-03 23:44:56 +0530

    Implement PDF upload endpoint and local file storage

--- app/main.py
+++ app/main.py
@@ -1,17 +1,38 @@
-from fastapi import FASTAPI
+from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
+from pathlib import Path
+import shutil
 
-app = FASTAPI()
+app = FastAPI()
+
+UPLOAD_FOLDER = Path("uploads")
+UPLOAD_FOLDER.mkdir(exist_ok=True)
+
 
 class Question(BaseModel):
     question: str
 
+
 @app.get("/")
 def home():
-    return {"Message":"Hello RAG"}
+    return {"message": "Hello RAG"}
+
 
 @app.post("/ask")
 def ask(question: Question):
     return {
-        "received_question": question.question,
+        "received_question": question.question
+    }
+
+
+@app.post("/upload")
+def upload_pdf(file: UploadFile = File(...)):
+    file_path = UPLOAD_FOLDER / file.filename
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "filename": file.filename,
+        "status": "uploaded successfully"
     }
